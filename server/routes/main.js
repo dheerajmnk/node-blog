079fda37b18021f8022aacb0afc35997174a15a6
Dheeraj Menon <dheerajmenonk@gmail.com>
2023-09-12 22:40:26 +0530

    Initial Commit

---
+++ server/routes/main.js
@@ -0,0 +1,127 @@
+const express = require('express');
+const router = express.Router();
+const Post = require('../models/Post');
+
+// Home (GET)
+router.get('', async (req, res) => {
+
+    try {
+        const locals = {
+            title: "Dheeraj's Tech Blog",
+            description: "Simple blog created with NodeJS, Express and MongoDB"
+        }
+
+        let perPage = 10;
+        let page = req.query.page || 1;
+
+        const data = await Post.aggregate([ { $sort: { createdAt: -1 } } ])
+            .skip(perPage * page - perPage)
+            .limit(perPage)
+            .exec();
+
+        const count = await Post.count();
+        const nextPage = parseInt(page) + 1;
+        const hasNextPage = nextPage <= Math.ceil(count / perPage);
+
+        res.render('index', { 
+            locals, 
+            data,
+            current: page,
+            nextPage: hasNextPage ? nextPage : null,
+            currentRoute: '/'
+        });
+
+    } catch (error) {
+        console.log(error);
+    }
+    
+});
+
+// Post - id (GET)
+router.get('/post/:id', async (req, res) => {
+    try {
+      let slug = req.params.id;
+  
+      const data = await Post.findById({ _id: slug });
+  
+      const locals = {
+        title: data.title,
+        description: "Simple Blog created with NodeJs, Express & MongoDb.",
+        currentRoute: `/post/${slug}`
+      }
+  
+      res.render('post', { 
+        locals,
+        data,
+        currentRoute: `/post/${slug}`
+      });
+    } catch (error) {
+      console.log(error);
+    }
+  
+});
+
+// Post - search (POST)
+router.post('/search', async (req, res) => {
+    try {
+      const locals = {
+        title: "Seach",
+        description: "Simple Blog created with NodeJs, Express & MongoDb."
+      }
+  
+      let searchTerm = req.body.searchTerm;
+      const searchNoSpecialChar = searchTerm.replace(/[^a-zA-Z0-9 ]/g, "")
+  
+      const data = await Post.find({
+        $or: [
+          { title: { $regex: new RegExp(searchNoSpecialChar, 'i') }},
+          { body: { $regex: new RegExp(searchNoSpecialChar, 'i') }}
+        ]
+      });
+  
+      res.render("search", {
+        data,
+        locals,
+        currentRoute: '/'
+      });
+  
+    } catch (error) {
+      console.log(error);
+    }
+  
+  });
+  
+
+router.get('/about', (req, res) => {
+    res.render('about', {
+    currentRoute: '/about'
+  });
+});
+
+router.get('/contact', (req, res) => {
+    res.render('contact', {
+      currentRoute: '/contact'
+    });
+});
+
+
+module.exports = router
+
+// function insertPostData () {
+//     Post.insertMany([
+//         {
+//             title: "Building a blog",
+//             body: "This is the body text"
+//         },
+//         {
+//             title: "Building a blog",
+//             body: "This is the body text"
+//         },
+//         {
+//             title: "Building a blog",
+//             body: "This is the body text"
+//         }
+//     ])
+// }
+
+// insertPostData();
